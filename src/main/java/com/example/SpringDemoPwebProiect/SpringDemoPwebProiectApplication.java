e9d461f1a5ba25159deb7d47fc48f68a86a92827
botosrebeka <101407780+botosrebeka@users.noreply.github.com>
2022-05-23 21:25:46 +0300

    first

---
+++ src/main/java/com/example/SpringDemoPwebProiect/SpringDemoPwebProiectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoPwebProiect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoPwebProiectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoPwebProiectApplication.class, args);
+	}
+
+}
