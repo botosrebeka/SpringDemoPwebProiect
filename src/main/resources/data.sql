a71be4cebbfacc51c1b43437b1f5523f392a5e52
botosrebeka <101407780+botosrebeka@users.noreply.github.com>
2022-05-25 13:15:26 +0300

    db conectat

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into t_book(title,author,isbn,release_date) values ('asd','asd',12345,'asd')
+insert into t_book(title,author,isbn,release_date) values ('asd','asd',12345,'asd')
+insert into t_book(title,author,isbn,release_date) values ('asd','asd',12345,'asd')
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+create table t_book
+(
+    id integer auto_increment,
+    title varchar(50),
+    author varchar(50),
+    isbn integer,
+    release_date varchar(50),
+    primary key(id)
+);
